d8e1081a60499b1b30f9e0cb90ec6b2f755fd360
Joris <jor.pell@hotmail.fr>
2020-05-31 23:21:11 +0200

    test avec struct

--- TB_modules/max7219_checker.sv
+++ TB_modules/max7219_checker.sv
@@ -10,6 +10,47 @@ module max7219_checker
     );
 
 
+   // STRUCTURE
+
+   typedef enum logic[3:0]
+   {
+    NO_OP        = 4'h0,
+    DIGIT_0      = 4'h1,
+    DIGIT_1      = 4'h2,
+    DIGIT_2      = 4'h3,
+    DIGIT_3      = 4'h4,
+    DIGIT_4      = 4'h5,
+    DIGIT_5      = 4'h6,
+    DIGIT_6      = 4'h7,
+    DIGIT_7      = 4'h8,
+    DECODE_MODE  = 4'h9,
+    INTENSITY    = 4'hA,
+    SCAN_LIMIT   = 4'hB,
+    SHUTDOWN     = 4'hC,
+    DISPLAY_TEST = 4'hF
+    } max7219_reg_addr_t;
+
+   typedef struct packed
+   {
+      logic [7:0] REG_NO_OP;
+      logic [7:0] REG_DIGIT_0;
+      logic [7:0] REG_DIGIT_1;
+      logic [7:0] REG_DIGIT_2;
+      logic [7:0] REG_DIGIT_3;
+      logic [7:0] REG_DIGIT_4;
+      logic [7:0] REG_DIGIT_5;
+      logic [7:0] REG_DIGIT_6;
+      logic [7:0] REG_DIGIT_7;
+      logic [7:0] REG_DECODE_MODE;
+      logic [7:0] REG_SCAN_LIMIT;
+      logic [7:0] REG_SHUTDOWN;
+      logic [7:0] REG_DISPLAY_TEST;
+      
+   } max7219_register_struct_t;
+   
+    max7219_register_struct_t matrix_0_reg;
+		
+   
    // INTERNAL WIRE
    logic    [15:0] s_max7219_data;
    logic    [3:0]  s_cnt_15;
@@ -107,9 +148,10 @@ module max7219_checker
    always @(posedge clk) begin
       if(!rst_n) begin
 	 
-	 for(i = 0; i < G_DIGITS_NB - 1 ; i++) begin
+	 for(i = 0; i < G_DIGITS_NB ; i++) begin
 	    s_config_after_load[i] <= 0;
 	 end	 
+	 matrix_0_reg.REG_NO_OP = 0;
 	 
       end
       else begin
@@ -120,6 +162,12 @@ module max7219_checker
 	 end
 
 	 if(i_max7219_load) begin
+	    case (s_config_after_load[11:8])
+              //4'h0 : matrix_0_reg.REG_NO_OP = s_max7219_data[7:0];
+	      
+	      default : $display("Error");
+	      
+	    endcase // case (s_config_after_load[11:8])
 	    
 	    
 	 end	 	   
