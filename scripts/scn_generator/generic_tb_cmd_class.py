8ff1caf08281b20b684ff8a0689d2ee2f5e3232d
JorisP <jor.pell@hotmail.fr>
2021-03-02 22:20:07 +0100

    up cmd

--- scripts/scn_generator/generic_tb_cmd_class.py
+++ scripts/scn_generator/generic_tb_cmd_class.py
@@ -6,7 +6,6 @@
 # Version : 1.0
 #
 #
-# Example 1 : 
 
 import sys
 
@@ -23,10 +22,74 @@ class generic_tb_cmd_class:
     # Print the SET command with Data in HEXA
     # data : integer
     def SET(self, alias, data):
-
         line_to_print = "SET " + alias + " " + hex(data) + "\n"
         self.f.write(line_to_print)
 
+        
+
+    # Print WTR commands
+    # Timeout is optionnal
+    def WTR(self, alias, timeout = 'none', unity = 'none'):
+        if(timeout == 'none' and unity == 'none'):            
+            line_to_print = "WTR " + alias + "\n"
+            self.f.write(line_to_print)
+        else:
+             line_to_print = "WTR " + alias + " " + str(timeout) + " " + unity + "\n"
+             self.f.write(line_to_print)
+
+
+    # Print WTF commands
+    # Timeout is optionnal
+    def WTF(self, alias, timeout = 'none', unity = 'none'):
+        if(timeout == 'none' and unity == 'none'):            
+            line_to_print = "WTF " + alias + "\n"
+            self.f.write(line_to_print)
+        else:
+             line_to_print = "WTF " + alias + " " + str(timeout) + " " + unity + "\n"
+             self.f.write(line_to_print)
+
+
+    # Print WTRS commands
+    # Timeout is optionnal
+    def WTRS(self, alias, timeout = 'none', unity = 'none'):
+        if(timeout == 'none' and unity == 'none'):            
+            line_to_print = "WTRS " + alias + "\n"
+            self.f.write(line_to_print)
+        else:
+             line_to_print = "WTRS " + alias + " " + str(timeout) + " " + unity + "\n"
+             self.f.write(line_to_print)
+
+
+    # Print WTFS commands
+    # Timeout is optionnal
+    def WTFS(self, alias, timeout = 'none', unity = 'none'):
+        if(timeout == 'none' and unity == 'none'):            
+            line_to_print = "WTFS " + alias + "\n"
+            self.f.write(line_to_print)
+        else:
+             line_to_print = "WTFS " + alias + " " + str(timeout) + " " + unity + "\n"
+             self.f.write(line_to_print)
+
+    # Print CHK Commands
+    # test : "OK" or "ERROR"
+    def CHK(self, alias, data, test):
+        line_to_print = "CHK " + alias + " " + hex(data) + " " + test + "\n"
+        self.f.write(line_to_print)
+
+        
+
+    # Print WAIT commands
+    # duree : int
+    # unity : "ps", "ns", "us", "ms"
+    def WAIT(self, duree, unity):
+        line_to_print = "WAIT " + str(duree) + " " + unity + "\n"
+        self.f.write(line_to_print)
+
+
+    # Print Modelsim Command
+    def MODELSIM_CMD(self, modelsim_cmd):
+        line_to_print = "MODELSIM_CMD" + "(\"" + modelsim_cmd + "\")" + "\n"
+        self.f.write(line_to_print)
 
     # Print END_TEST at the end of the test
     def END_TEST(self):#, self.out_file):
--- scripts/scn_generator/test.py
+++ scripts/scn_generator/test.py
@@ -17,5 +17,75 @@ generic_cmd_scn_xx.print_line("-- \n")
 for i in range(0, 100):
     generic_cmd_scn_xx.generic_tb_cmd.SET("TOTO", i)
 
+generic_cmd_scn_xx.print_line("\n")
+
+generic_cmd_scn_xx.print_line("\n")
+
+
+generic_cmd_scn_xx.generic_tb_cmd.WTR("TOTO", 10, "ps")
+generic_cmd_scn_xx.generic_tb_cmd.WTR("TOTO", 10, "ns")
+generic_cmd_scn_xx.generic_tb_cmd.WTR("TOTO", 10, "us")
+generic_cmd_scn_xx.generic_tb_cmd.WTR("TOTO", 10, "ms")
+generic_cmd_scn_xx.generic_tb_cmd.WTR("TOTO", 666, "ps")
+generic_cmd_scn_xx.generic_tb_cmd.WTR("TOTO", 666, "ns")
+generic_cmd_scn_xx.generic_tb_cmd.WTR("TOTO", 666, "us")
+generic_cmd_scn_xx.generic_tb_cmd.WTR("TOTO", 666, "ms")
+generic_cmd_scn_xx.generic_tb_cmd.WTR("TOTO", 56, "ps")
+generic_cmd_scn_xx.generic_tb_cmd.WTR("TOTO", 56, "ns")
+generic_cmd_scn_xx.generic_tb_cmd.WTR("TOTO", 56, "us")
+generic_cmd_scn_xx.generic_tb_cmd.WTR("TOTO", 56, "ms")
+
+generic_cmd_scn_xx.generic_tb_cmd.WTF("TOTO", 10, "ps")
+generic_cmd_scn_xx.generic_tb_cmd.WTF("TOTO", 10, "ns")
+generic_cmd_scn_xx.generic_tb_cmd.WTF("TOTO", 10, "us")
+generic_cmd_scn_xx.generic_tb_cmd.WTF("TOTO", 10, "ms")
+generic_cmd_scn_xx.generic_tb_cmd.WTF("TOTO", 666, "ps")
+generic_cmd_scn_xx.generic_tb_cmd.WTF("TOTO", 666, "ns")
+generic_cmd_scn_xx.generic_tb_cmd.WTF("TOTO", 666, "us")
+generic_cmd_scn_xx.generic_tb_cmd.WTF("TOTO", 666, "ms")
+generic_cmd_scn_xx.generic_tb_cmd.WTF("TOTO", 56, "ps")
+generic_cmd_scn_xx.generic_tb_cmd.WTF("TOTO", 56, "ns")
+generic_cmd_scn_xx.generic_tb_cmd.WTF("TOTO", 56, "us")
+generic_cmd_scn_xx.generic_tb_cmd.WTF("TOTO", 56, "ms")
+
+
+generic_cmd_scn_xx.generic_tb_cmd.WTRS("TOTO", 10, "ps")
+generic_cmd_scn_xx.generic_tb_cmd.WTRS("TOTO", 10, "ns")
+generic_cmd_scn_xx.generic_tb_cmd.WTRS("TOTO", 10, "us")
+generic_cmd_scn_xx.generic_tb_cmd.WTRS("TOTO", 10, "ms")
+generic_cmd_scn_xx.generic_tb_cmd.WTRS("TOTO", 666, "ps")
+generic_cmd_scn_xx.generic_tb_cmd.WTRS("TOTO", 666, "ns")
+generic_cmd_scn_xx.generic_tb_cmd.WTRS("TOTO", 666, "us")
+generic_cmd_scn_xx.generic_tb_cmd.WTRS("TOTO", 666, "ms")
+generic_cmd_scn_xx.generic_tb_cmd.WTRS("TOTO", 56, "ps")
+generic_cmd_scn_xx.generic_tb_cmd.WTRS("TOTO", 56, "ns")
+generic_cmd_scn_xx.generic_tb_cmd.WTRS("TOTO", 56, "us")
+generic_cmd_scn_xx.generic_tb_cmd.WTRS("TOTO", 56, "ms")
+
+generic_cmd_scn_xx.generic_tb_cmd.WTFS("TOTO", 10, "ps")
+generic_cmd_scn_xx.generic_tb_cmd.WTFS("TOTO", 10, "ns")
+generic_cmd_scn_xx.generic_tb_cmd.WTFS("TOTO", 10, "us")
+generic_cmd_scn_xx.generic_tb_cmd.WTFS("TOTO", 10, "ms")
+generic_cmd_scn_xx.generic_tb_cmd.WTFS("TOTO", 666, "ps")
+generic_cmd_scn_xx.generic_tb_cmd.WTFS("TOTO", 666, "ns")
+generic_cmd_scn_xx.generic_tb_cmd.WTFS("TOTO", 666, "us")
+generic_cmd_scn_xx.generic_tb_cmd.WTFS("TOTO", 666, "ms")
+generic_cmd_scn_xx.generic_tb_cmd.WTFS("TOTO", 56, "ps")
+generic_cmd_scn_xx.generic_tb_cmd.WTFS("TOTO", 56, "ns")
+generic_cmd_scn_xx.generic_tb_cmd.WTFS("TOTO", 56, "us")
+generic_cmd_scn_xx.generic_tb_cmd.WTFS("TOTO", 56, "ms")
+
+generic_cmd_scn_xx.generic_tb_cmd.CHK("TITI", 0xFEFEFEFE, "ERROR")
+generic_cmd_scn_xx.generic_tb_cmd.CHK("TITI", 0xFEFEFEF0, "ERROR")
+generic_cmd_scn_xx.generic_tb_cmd.CHK("TITI", 0xFEFEFEF8, "ERROR")
+generic_cmd_scn_xx.generic_tb_cmd.CHK("TITI", 0xFEFEFEF6, "ERROR")
+generic_cmd_scn_xx.generic_tb_cmd.CHK("TITI", 0xFEFEFEF4, "ERROR")
+
+generic_cmd_scn_xx.generic_tb_cmd.MODELSIM_CMD("force - freeze ...")
+
+
 generic_cmd_scn_xx.print_line("-- END OF SIMULATION\n\n")
+
+
+
 generic_cmd_scn_xx.END_TEST()
