6b2cd1aaf3e757cdfe0ef34ea1f54ca28de44a2a
JorisP <jor.pell@hotmail.fr>
2021-04-17 00:38:55 +0200

    CLASS TB UART todo

---
+++ lib_tb_uart/tb_uart_class.sv
@@ -0,0 +1,11 @@
+//                              -*- Mode: Verilog -*-
+// Filename        : tb_uart_class.sv
+// Description     : Testbench UART class
+// Author          : JorisP
+// Created On      : Sat Apr 17 00:36:04 2021
+// Last Modified By: JorisP
+// Last Modified On: Sat Apr 17 00:36:04 2021
+// Update Count    : 0
+// Status          : Unknown, Use with caution!
+
+// TBD !
