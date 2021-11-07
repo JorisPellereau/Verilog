1c2af05616aff20414c504bd69a9f1fa4c302498
JorisP <jor.pell@hotmail.fr>
2021-11-07 20:15:04 +0100

    Modif en cours - add data_collector en cours

--- lib_tb_data_collector/tb_data_collector_class.sv
+++ lib_tb_data_collector/tb_data_collector_class.sv
@@ -13,6 +13,26 @@ class tb_data_collector_class #(
 				parameter G_DATA_WIDTH   = 32
 				);
 
+   // == Struct ==
+   // Struct for Command and number of Arg
+   struct {      
+      string cmd;
+      int    arg_number;
+   } cmd_struct;
+   // ============
+
+   // == INFOS. ==
+   string tb_module_cmd; // Name of Testbench Module to be use in scenario
+   
+   // Associative array of DATA_COLLECTOR Commands
+   int 	  DATA_COLLECTOR_CMD_ARRAY [string];
+
+   cmd_struct init_cmd;
+   cmd_struct start_cmd;
+   cmd_struct stop_cmd;
+   cmd_struct close_cmd;   
+   // ============
+
    // == VIRTUAL I/F ==
    // DATA_COLLECTOR interface
    virtual data_collector_intf #(G_NB_COLLECTOR, G_DATA_WIDTH) data_collector_vif;   
@@ -20,7 +40,20 @@ class tb_data_collector_class #(
 
    // == Interface passed in Virtual I/F ==
    function new(virtual data_collector_intf #(G_NB_COLLECTOR, G_DATA_WIDTH) data_collector_nif);
-      this.data_collector_vif = data_collector_nif; // New Virtual Interface      
+      this.data_collector_vif = data_collector_nif; // New Virtual Interface
+      this.tb_module_cmd = "DATA_COLLECTOR";
+
+      // List of Command of DATA_COLLECTOR
+      this.DATA_COLLECTOR_CMD_ARRAY = '{
+					"INIT"  : 0,
+					"START" : 1,
+					"STOP"  : 2,
+					"CLOSE" : 3
+					};
+
+      this.init_cmd.cmd = "INIT";
+      this.init_cmd.arg_number = 2;
+      
    endfunction // new   
    // =====================================
 
@@ -34,12 +67,11 @@ class tb_data_collector_class #(
    // DATA_COLLECTOR[alias] CLOSE(i)      // Close collect file for alias index i
    // =====================================
 
-   // Associative array of DATA_COLLECTOR Commands
-   int DATA_COLLECTOR_CMD_ARRAY [string] = '{
-					     "INIT"  : 0,
-					     "START" : 1,
-					     "STOP"  : 2,
-					     "CLOSE" : 3
-					     };
+
+   // == ADD ALIAS in Associative Array ==
+   function void DATA_COLLECTOR_TB_ADD_ALIAS(string ALIAS, int alias_index);
+      this.data_collector_vif.data_collector_alias[ALIAS] = alias_index;
+   endfunction // DATA_COLLECTOR_TB_ADD_ALIAS   
+   // ====================================
    
 endclass // tb_data_collector_class
--- lib_tb_generic/tb_generic_class.sv
+++ lib_tb_generic/tb_generic_class.sv
@@ -10,15 +10,67 @@
 
 class tb_generic_class;
 
-   // == Task Decode Scenario Line ==
-   task decod_scn_line(input string line,
-		       input int     tb_module_cmd_list [string],
+   // Struct for Command and number of Arg
+   struct {      
+      string cmd;
+      int    arg_number;
+   } cmd_struct;
+   
 
-		       output logic  o_cmd_exist,
-		       output string o_alias,
-		       output string o_cmd,
-		       output string o_cmd_args);
+   // == Task Decode Scenario Line ==
+   task decod_scn_line(input string  line,                     // Current line to decod
+		       input int  tb_module_cmd [string],      // Command of the Module (UART[..] , DATA_COLLECTOR[..] etc)
+		       input int  alias_list[string],          // Associative array with ALIASES
+		       input int  tb_module_cmd_list [string], // Associative Array with commands of current TB module
+
+		       output logic  o_cmd_exist, // A flag that indicate s if the current command exists
+		       output string o_alias,     // The return Alias
+		       output string o_cmd,       // Decoded command
+		       output int o_cmd_args[string]); // Args of the commands
       begin
+
+	 // Internal Variables
+	 int tb_module_cmd_len    = 0; // Len of tb_module_cmd
+	 int line_length          = 0; // Length of the line
+	 int pos_0                = 0; // Position of [ character
+	 int pos_1                = 0; // Position of ] character
+	 int first_space_position = 0; // First position of " " character
+	 
+	 int i; // Loop Index
+
+	 string alias_str; // Extracted Alias of the line
+	 
+	 
+	 // Get the length of the tb_module_cmd
+	 tb_module_cmd_len = tb_module_cmd.len();
+
+	 // Get the length of the line
+	 line_length = line.len();
+	 
+
+	 
+	 // Check if command exists
+	 if(line.substr(0, tb_module_cmd_len - 1) == tb_module_cmd) begin
+
+	    // Get alias between [] and check if alias exist	    
+	    for(i = 0; i < line_length; i++) begin
+	       if(line.getc(i) == "[") begin
+		  pos_0 = i;		  
+	       end
+	       
+	       if(line.getc(i) == "]") begin
+		  pos_1 = i;		  
+	       end	       
+	    end
+
+	    // Get Alias
+	    alias_str = line.substr(pos_0 + 1, pos_1 - 1); // RM "[" and "]"
+
+	    // Get First space position
+	    first_space_position = pos_1 + 1;
+	    
+	    
+	 end
 	 
       end
    endtask // decod_scn_line
